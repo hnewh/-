837d01e066ebb3a6fe43eedc20b62a1e296446e7
hnewh <colin1101@naver.com>
2018-10-04 00:28:55 +0900

    complete

--- app.js
+++ app.js
@@ -1,54 +1,64 @@
 // 데이터 링크
-link = 'https://api.thingspeak.com/channels/575240/feeds.json?api_key=VCSZ7S7QTJ49A1FN&results=2';
+link = 'https://api.thingspeak.com/channels/575626/feeds.json?api_key=EUL7KB1QYEAQQX0S&results=2';
 
+// 열려있는 시간
+var time;
 // 전기사용량, 전기요금, 탄소배출량 총합
-esum = 0, bsum = 0, csum = 0;
+var esum = 0, bsum = 0, csum = 0;
 // 제품번호 배열
 product = new Array();
 
-// 15초에 한 번 데이터 링크 로드
-check = setInterval(function(event){
-    $('#check').load(link, function(data){
-        // json으로 data불러오기
-        var obj = JSON.parse(data);
-        
-        // 태그, 열린 시간 불러오기
-        tag = parseFloat(obj["feeds"][0][5]);
-        time = parseFloat(obj["feeds"][0][6]); 
+$(window).ready(function(event){
+    check = setInterval(function(event){
+        $('#check').load(link, function(data){
+            // json으로 data불러오기
+            var obj = JSON.parse(data);
 
-        // 총합 계산
-        esum += time.toFixed(2);
-        bsum += (time * 0.004).toFixed(2);
-        csum += (time * 4.24).toFixed(2);
+            // 태그, 열린 시간 불러오기
+            tag = parseFloat(obj["feeds"][0]["field1"]);
+            time = parseFloat(obj["feeds"][0]["field2"]);
 
-        // check.html에 추가
-        $('#section-list #elec').append("<div class='info middle'>총 사용량 : " + esum + "Wh</div>");
-        $('#section-list #Bill').append("<div class='info middle'>총 요금 : " + bsum + "원</div>");
-        $('#section-list #Cb').append("<div class='info middle'>총 배출량 : " + csum + "TC</div>");
-        $('#section-list #time').append("<div class='info middle'>열린 시간: " + time + "초</div>");
+            // 현재 시간과 열린 시간 check.html에 추가
+            var date = new Date();
+            var dateStr = date.getFullYear() + "년 " + date.getMonth() + "월 " + date.getDate() + "일 " + date.getHours() + "시 " + date.getMinutes() + "분 " + date.getSeconds() + "초";
+            $('#section-list #time .sub-title').html(dateStr + " - " + time + "초");
 
-        // 제품 배열 확인하고 check.html에 추가
-        if(checkProduct(tag) == 1)
+            // 제품 배열 확인하고 check.html에 추가
+            if(checkProduct(tag) == 1)
+            {
+                product.append(tag);
+                $('#section-list #rfid .sub-title').html(tag + "번 제품이 냉장고 안에 들어갔습니다.");
+            }
+            else if(checkProduct(tag) == -1)
+                $('#section-list #rfid .sub-title').html(tag + "번 제품이 냉장고 밖으로 나갔습니다.");
+            else if(checkProduct(tag) == 0)
+                $('#section-list #rfid .sub-title').html("냉장고 안에 아무 제품도 들어있지 않습니다.");
+        });
+
+        if(typeof(time) == 'number')
         {
-            product.append(tag);
-            $('#section-list #rfid').append("<div class='info middle'>" + tag + "번 제품이 냉장고 안에 들어갔습니다.</div>");
+            // 총합 계산
+            esum = parseFloat((time * 0.027).toFixed(3));
+            bsum = parseFloat((time * 0.004).toFixed(3));
+            csum = parseFloat((time * 4.24).toFixed(3));
+        
+            str1 = esum + "W";
+            str2 = bsum + "원";
+            str3 = csum + "g";
+
+            // check.html에 추가
+            $('#section-list #elec .sub-title').html(str1);
+            $('#section-list #Bill .sub-title').html(str2);
+            $('#section-list #Cb .sub-title').html(str3);
         }
-        else if(checkProduct(tag) == -1)
-            $('#section-list #rfid').append("<div class='info middle'>" + tag + "번 제품이 냉장고 밖으로 나갔습니다.</div>");
-        else
-            $('#section-list #rfid').append("<div class='info middle'>냉장고 안에 아무 제품도 들어있지 않습니다.</div>");
-    });
-}, 15000);
 
-remove = setInterval(function(event){
-    $('#section-list .info').remove();
-    esum = 0, bsum = 0, csum = 0, time = 0;
-}, 45000);
+    }, 3000);
 
-// 그래프 삽입
-$('#temp .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
-$('#mois .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
-$('#oxy .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
+    // 그래프 삽입
+    $('#temp .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
+    $('#mois .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
+    $('#oxy .graph').append("<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/575240/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15'></iframe>");
+});
 
 //상품 중복 확인
 function checkProduct(value)
